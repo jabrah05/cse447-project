d3a4831ad31e078bf85e046e4b7b790d28c2ecaa
John Abraham <johnabraham@creatorsagi.com>
2026-01-25 20:13:27 -0800

    Update team member info submit.sh

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "John Abraham,jabrah05\nJack Paul Armon,jarmon01\nAdonyas Argaw,aargaw01" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
